e2f487c0f66a32857b60ff7b2e81a0239d5f224a
cong-create <2214896872@qq.com>
2020-04-23 09:48:21 +0800

    修改了提示

--- app/src/main/java/com/jintingbo/p241_2/MyDatabaseHelper.java
+++ app/src/main/java/com/jintingbo/p241_2/MyDatabaseHelper.java
@@ -34,7 +34,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mcontex,"数据表建好了",Toast.LENGTH_LONG).show();
+        Toast.makeText(mcontex,"好了" ,Toast.LENGTH_LONG).show();
     }
 
     @Override
